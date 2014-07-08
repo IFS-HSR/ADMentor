ef5920c3ce38f544357e788e017f284d0acd50bb
Lukas Wegmann <wegmaluk@gmail.com>
2014-07-08 14:29:51 +0200

    move technology to technology package

--- ADAddIn/ADAddIn.cs
+++ ADAddIn/ADAddIn.cs
@@ -17,31 +17,7 @@ namespace AdAddIn
     {
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
-        private static readonly MDGTechnology technology = new MDGTechnology(
-            id: "ADMentor",
-            name: "AD Mentor",
-            description: "Modelling Language for documentation and reuse of architectural decisions",
-            diagrams: new Diagram[]{
-                ADTechnology.Diagrams.ProblemSpace,
-                ADTechnology.Diagrams.SolutionOverview
-            },
-            modelTemplates: new ModelTemplate[]{
-                new ModelTemplate(
-                    name: "Problem Space",
-                    description: "",
-                    icon: ModelIcon.ComponentModel,
-                    resourceName: "AdAddIn.ADTechnology.ProblemSpaceTemplate.xml"),
-                new ModelTemplate(
-                    name: "Solution Overview",
-                    description: "",
-                    icon: ModelIcon.DynamicModel,
-                    resourceName: "AdAddIn.ADTechnology.SolutionOverviewTemplate.xml"),
-                new ModelTemplate(
-                    name: "AD Mentor Demo",
-                    description: "",
-                    icon: ModelIcon.SimpleModel,
-                    resourceName: "AdAddIn.ADTechnology.DemoTemplate.xml")
-            });
+        private static readonly MDGTechnology technology = ADTechnology.Technologies.AD;
 
         public override string AddInName
         {
---
+++ ADAddIn/ADTechnology/Technology.cs
@@ -0,0 +1,38 @@
+﻿using EAAddInFramework.MDGBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdAddIn.ADTechnology
+{
+    public static class Technologies
+    {
+        public static readonly MDGTechnology AD = new MDGTechnology(
+            id: "ADMentor",
+            name: "AD Mentor",
+            description: "Modelling Language for documentation and reuse of architectural decisions",
+            diagrams: new Diagram[]{
+                        ADTechnology.Diagrams.ProblemSpace,
+                        ADTechnology.Diagrams.SolutionOverview
+                    },
+            modelTemplates: new ModelTemplate[]{
+                        new ModelTemplate(
+                            name: "Problem Space",
+                            description: "",
+                            icon: ModelIcon.ComponentModel,
+                            resourceName: "AdAddIn.ADTechnology.ProblemSpaceTemplate.xml"),
+                        new ModelTemplate(
+                            name: "Solution Overview",
+                            description: "",
+                            icon: ModelIcon.DynamicModel,
+                            resourceName: "AdAddIn.ADTechnology.SolutionOverviewTemplate.xml"),
+                        new ModelTemplate(
+                            name: "AD Mentor Demo",
+                            description: "",
+                            icon: ModelIcon.SimpleModel,
+                            resourceName: "AdAddIn.ADTechnology.DemoTemplate.xml")
+                    });
+    }
+}
