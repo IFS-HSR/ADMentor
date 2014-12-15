dedb089e392fc843f9bb3e6ad9940118d5a2f9bb
Lukas Wegmann <wegmaluk@gmail.com>
2014-12-15 17:20:41 +0100

    more detailed description of the diagram types

--- ADAddIn/ADTechnology/DiagramTypes.cs
+++ ADAddIn/ADTechnology/DiagramTypes.cs
@@ -12,21 +12,24 @@ namespace AdAddIn.ADTechnology
         public static readonly Diagram ProblemSpace = new Diagram(
             name: "ProblemSpace",
             displayName: "Problem Space",
-            description: "Models known issues and their relationship",
+            description: "ADMentor Problem Space Diagrams visualize recurring architectural design problems and known solutions to them. " +
+                         "They also visualize the relationships between these model elements.",
             type: DiagramType.Activity,
             toolbox: Toolboxes.ProblemSpaceTools);
 
         public static readonly Diagram QOC = new Diagram(
             name: "QOC",
             displayName: "Questions, Options and Criterions",
-            description: "Models questions, options and criterions",
+            description: "Question, Options and Criterions (QOC) Diagrams visualize recurring architectural design problems similar to Problem Space Diagrams." + 
+                         "Additionally, they support detailed assessments based on optional criterions.",
             type: DiagramType.Activity,
             toolbox: Toolboxes.ProblemSpaceTools);
 
         public static readonly Diagram SolutionOverview = new Diagram(
             name: "SolutionOverview",
             displayName: "Solution Overview",
-            description: "Models problems occured during a project and how they have been addressed",
+            description: "ADMentor Solution Space Diagrams collect architectural design problems that occured on a particular project and " +
+                         "the design options investigated and chosen to address them.",
             type: DiagramType.Activity,
             toolbox: Toolboxes.SolutionSpaceTools);
     }
